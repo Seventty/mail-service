27f390cfa248ad70a15320ad402aa2f7db0029ad
rainiery valerio <rainiery.valerio@economia.gob.do>
2024-10-31 17:06:50 -0400

    service and controller updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Email service')
+    .setDescription('Service to send emails')
+    .setVersion('1.0')
+    .addTag('email')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
